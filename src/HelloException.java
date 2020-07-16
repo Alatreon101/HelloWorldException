b21113139b38fa048e6fb0243e6482086b139b78
FASfalcon <alexanderfedasov@gmail.com>
2020-07-16 18:01:05 +0300

    HWE update1.1 - свой Exception

--- src/HelloException.java
+++ src/HelloException.java
@@ -1,19 +1,15 @@
+package src;
+
 import java.util.Scanner;
 
-public class HelloException {
+class HelloException extends Exception {
 
-
-    public static void main(String[] args) throws ArithmeticException {
-        //System.out.println("Hello World!");
+    public static void main(String[] args) throws HelloException {
 
         Scanner sc = new Scanner(System.in);
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
-
-        if (number2 == 0) {
-            System.out.println("Деление на ноль");
-            throw new ArithmeticException();
-        }
+        if (number2 == 0) throw new HelloException();
         int result = number1 / number2;
         System.out.println(result);
     }
